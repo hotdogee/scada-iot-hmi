7ca230b87e4e3cfc48f4b8e8cc7a57edeadd96e1
Han Lin <hotdogee@gmail.com>
2018-11-26 20:57:49 +0800

    Fix https

--- src/api/index.js
+++ src/api/index.js
@@ -3,15 +3,15 @@ import socketio from 'feathers-socketio/client'
 import feathers from 'feathers/client'
 
 // let apiUrl = 'http://localhost:8081'
-let apiUrl = 'https://scada.hanl.in'
+let apiUrl = 'https://scada.hanl.in/api/'
 let path = '/api/'
 if (process.env.NODE_ENV == 'production') {
-  apiUrl = 'https://scada.hanl.in'
+  apiUrl = 'https://scada.hanl.in/api/'
   path = '/api/'
 }
 export const url = apiUrl
 
-export const socket = io(url, { path })
+export const socket = io(url, { path: path })
 
 const api = feathers().configure(socketio(socket, { timeout: 10000 }))
 
