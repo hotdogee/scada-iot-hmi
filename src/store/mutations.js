c3f2540480a63412839b04a0b03ecbc00baf149c
Han Lin <hotdogee@gmail.com>
2019-05-16 20:06:52 +0800

    Added computed values

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import util from 'util'
 import * as types from './mutation-types'
+import { IAPWS97_EoS } from '@neutrium/thermo.eos.iapws97'
 
 export default {
   [types.SET_LOGS] (state, { logs }) {
@@ -101,6 +102,160 @@ function addLog (state, log) {
         state.chartData[header].shift()
       }
     })
+  })
+  // computed
+  let addrName = 'M200-計算'
+  if (!state.cardData[addrName]) {
+    state.cardData[addrName] = {}
+  }
+  // calculate extra series
+  const m73W = state.chartData['M73-發電機300kVA-有功功率(W)']
+  const m73VA = state.chartData['M73-發電機300kVA-視在功率(VA)']
+  const m72W = state.chartData['M72-併接點-有功功率(W)']
+  // const m13bar = state.chartData['M13-渦輪1前-壓力(bar)']
+  // const m14bar = state.chartData['M14-渦輪1後-壓力(bar)']
+  const m25tph = state.chartData['M25-主排水管-質量流率(t/h)']
+  const m1bar = state.chartData['M1-九號井口-壓力(bar)']
+  const m1c = state.chartData['M1-九號井口-溫度(℃)']
+  const m5bar = state.chartData['M5-渦輪2前-壓力(bar)']
+  const m5c = state.chartData['M5-渦輪2前-溫度(℃)']
+  const m6bar = state.chartData['M6-渦輪2後-壓力(bar)']
+  const m6c = state.chartData['M6-渦輪2後-溫度(℃)']
+  let m1e = null
+  if (m1bar && m1c) {
+    m1e = (new IAPWS97_EoS()).solve({ p: m1bar.slice(-1)[0].value * 100000, t: 273.15 + m1c.slice(-1)[0].value })
+  }
+  let m5e = null
+  if (m5bar && m5c) {
+    m5e = (new IAPWS97_EoS()).solve({ p: m5bar.slice(-1)[0].value * 100000, t: 273.15 + m5c.slice(-1)[0].value })
+  }
+  let m6e = null
+  if (m6bar && m6c) {
+    m6e = (new IAPWS97_EoS()).solve({ p: m6bar.slice(-1)[0].value * 100000, t: 273.15 + m6c.slice(-1)[0].value })
+  }
+  // const m25tph = state.chartData['M26-排水管2-流量(m3/h)']
+  // const m64hz = state.chartData['M64-發電機1-頻率(Hz)']
+  const m71hz = state.chartData['M71-發電機300kVA-頻率(Hz)']
+  const turbineRadius = 0.215 // m
+  const nozzleAngle = 30 / 180 * Math.PI // radians
+  const formulas = {
+    '出噴嘴速度': {
+      unit: 'm/s',
+      value: () => {
+        if (m73VA && m25tph && m71hz && m73VA.slice(-1)[0].value > 0 && m25tph.slice(-1)[0].value > 0 && m71hz.slice(-1)[0].value > 0) {
+          const omega = 2 * Math.PI * m71hz.slice(-1)[0].value
+          const torque = m73VA.slice(-1)[0].value / omega
+          const force = torque / turbineRadius
+          const kgs = m25tph.slice(-1)[0].value / 3.6
+          const turbineSpeed = turbineRadius * omega
+          const v2 = (force / (2 * kgs) + turbineSpeed) / Math.cos(nozzleAngle)
+          // const v2 = ((m63kW[stat] * 1000) / (m71hz * m25tph[stat] * 2 * Math.PI * 0.215 * 2  * 1000 / 3600) + 0.215 * 2 * Math.PI * m71hz) / Math.cos(30 / 180 * Math.PI)
+          // m63kW[stat] / (m71hz * m25tph[stat]) + m71hz
+          return v2
+        } else return -1
+      }
+    },
+    '併網效率': {
+      unit: '%',
+      value: () => {
+        if (m73W && m72W && m72W.slice(-1)[0].value > 0 && m73W.slice(-1)[0].value > 0) {
+          // console.log(m72W.slice(-1)[0].value, m73W.slice(-1)[0].value)
+          const eff = (m72W.slice(-1)[0].value + 3000) / m73W.slice(-1)[0].value
+          return eff > 1 ? 100 : eff * 100
+        } else return -1
+      }
+    },
+    '常數': {
+      unit: '', // kW/bar2
+      value: () => {
+        if (m73VA && m5bar && m6bar && (m5bar.slice(-1)[0].value - m6bar.slice(-1)[0].value) > 0) {
+          // console.log(m73VA, m5bar, m6bar)
+          const c = m73VA.slice(-1)[0].value / 1000 / Math.pow(m5bar.slice(-1)[0].value - m6bar.slice(-1)[0].value, 2)
+          return c
+        } else return -1
+      }
+    },
+    '井口質量焓': {
+      unit: 'kJ/kg',
+      value: () => {
+        if (m1e) {
+          return m1e.h
+        } else return -1
+      }
+    },
+    '入口質量焓': {
+      unit: 'kJ/kg',
+      value: () => {
+        if (m5e) {
+          return m5e.h
+        } else return -1
+      }
+    },
+    '出口質量焓': {
+      unit: 'kJ/kg',
+      value: () => {
+        if (m6e) {
+          return m6e.h
+        } else return -1
+      }
+    },
+    '熱效率(入)': {
+      unit: '%',
+      value: () => {
+        if (m5e && m25tph && m73VA) {
+          const kva = m73VA.slice(-1)[0].value / 1000
+          const kgs = m25tph.slice(-1)[0].value / 3.6
+          const hi = m5e.h
+          return kva / kgs / hi * 100
+        } else return -1
+      }
+    },
+    '熱效率(入-出)': {
+      unit: '%',
+      value: () => {
+        if (m5e && m6e && m25tph && m73VA) {
+          const kva = m73VA.slice(-1)[0].value / 1000
+          const kgs = m25tph.slice(-1)[0].value / 3.6
+          const hi = m5e.h
+          const ho = m6e.h
+          return kva / kgs / (hi - ho) * 100
+        } else return -1
+      }
+    }
+  }
+  _.forEach(Object.entries(formulas), ([regName, reg]) => {
+    if (!state.cardData[addrName][regName]) {
+      state.cardData[addrName][regName] = {
+        trend: []
+      }
+    }
+    const unit = prefixExp[reg.unit[0]] ? reg.unit.slice(1) : reg.unit
+    let exp = prefixExp[reg.unit[0]] || 0
+    const originalValue = reg.value()
+    let value = originalValue
+    while (value > 10000) {
+      value /= 1000
+      exp += 3
+    }
+    const prefix = expPrefix[exp] || ''
+    state.cardData[addrName][regName].value = value.toFixed(2)
+    state.cardData[addrName][regName].unit = prefix + unit
+    state.cardData[addrName][regName].trend.push(Math.round(originalValue * 10000) / 100)
+    while (state.cardData[addrName][regName].trend.length > limit) {
+      state.cardData[addrName][regName].trend.shift()
+    }
+    // 'M1-九號井口-溫度(°C)'
+    const header = util.format('%s-%s(%s)', addrName, regName, reg.unit)
+    if (!state.chartData[header]) {
+      state.chartData[header] = []
+    }
+    state.chartData[header].push({
+      value: originalValue,
+      unit
+    })
+    while (state.chartData[header].length > limit) {
+      state.chartData[header].shift()
+    }
   })
 }
 
