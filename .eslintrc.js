692bc38c1f92541e98d349b7abb9ccc8cb602cd1
Han Lin <hotdogee@gmail.com>
2019-07-18 19:33:14 +0800

    plugin:vue/recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,8 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     // "prettier", "prettier/standard",
-    'plugin:vue/essential',
+    // 'plugin:vue/essential',
+    'plugin:vue/recommended',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     '@vue/standard'
     // 'plugin:prettier/recommended',
