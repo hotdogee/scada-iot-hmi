b4f225acdb3700d40d264f7060dc3aae753608ef
Han Lin <hotdogee@gmail.com>
2025-04-17 22:34:03 +0800

    feat: enhance Feathers client setup with socket connection handling and logging

--- src/boot/feathers.ts
+++ src/boot/feathers.ts
@@ -1,13 +1,18 @@
 import { defineBoot } from '#q-app/wrappers'
 import { feathers } from '@feathersjs/feathers'
 import socketio from '@feathersjs/socketio-client'
+import Logger from 'assets/logger'
 import { io } from 'socket.io-client'
+import { useLogsStore } from '../stores/logs'
+const logger = new Logger('boot.feathers')
 
 // Setup the Feathers client
 const socket = io(process.env.API_URL, {
   path: process.env.API_PATH + '/socket.io/',
   // throw errors if the server doesn't respond in time
-  ackTimeout: 5000
+  ackTimeout: 5000,
+  // manually connect after the connect listener is registered
+  autoConnect: false
 })
 const client = feathers()
 // Set up Socket.io client with the socket
@@ -16,10 +21,20 @@ client.configure(socketio(socket))
 const logs = client.service('logs')
 const images = client.service('images')
 
-export default defineBoot(({ app }) => {
+export default defineBoot(({ app, store }) => {
   app.config.globalProperties.$client = client
   app.config.globalProperties.$logs = logs
   app.config.globalProperties.$images = images
+  socket.on('connect', async () => {
+    const logsStore = useLogsStore(store)
+    logger.info(`socket connected`, logsStore)
+    await logsStore.setupRealtimeUpdates()
+    await logsStore.findStartDateTime()
+  })
+  socket.connect()
+  socket.on('disconnect', () => {
+    logger.info(`disconnect`)
+  })
 })
 
-export { client, images, logs }
+export { client, images, logs, socket }
